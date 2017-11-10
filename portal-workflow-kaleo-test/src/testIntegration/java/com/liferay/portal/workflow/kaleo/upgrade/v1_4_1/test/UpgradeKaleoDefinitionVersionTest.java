brandizzi/liferay-portal

LPS-73734 Test SF: make DB instance a field, for a shorter code.

modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/upgrade/v1_4_1/test/UpgradeKaleoDefinitionVersionTest.java
@@ -65,6 +65,8 @@ public class UpgradeKaleoDefinitionVersionTest {
 		_name = StringUtil.randomString();
 		_timestamp = new Timestamp(System.currentTimeMillis());
 
+		_db = DBManagerUtil.getDB();
+
 		setUpUpgradeKaleoDefinitionVersion();
 	}
 
@@ -118,12 +120,11 @@ public class UpgradeKaleoDefinitionVersionTest {
 	}
 
 	protected void deleteKaleoDefinitionVersion(String name) throws Exception {
-		DB db = DBManagerUtil.getDB();
+		_db.runSQLTemplateString(
+			"delete from KaleoDefinitionVersion where name = '" + name + "'",
+			false, true);
 
-		db.runSQL(
-			"delete from KaleoDefinitionVersion where name = '" + name + "'");
-
-		db.runSQL("delete from KaleoDefinition where name = '" + name + "'");
+		_db.runSQL("delete from KaleoDefinition where name = '" + name + "'");
 	}
 
 	protected KaleoDefinitionVersion getKaleoDefinitionVersion(
@@ -169,6 +170,8 @@ public class UpgradeKaleoDefinitionVersionTest {
 
 			});
 	}
+
+	private DB _db;
 
 	@DeleteAfterTestRun
 	private Group _group;

inacionery/com-liferay-portal-workflow

portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/upgrade/v1_4_1/test/UpgradeKaleoDefinitionVersionTest.java
@@ -65,6 +65,8 @@ public class UpgradeKaleoDefinitionVersionTest {
 		_name = StringUtil.randomString();
 		_timestamp = new Timestamp(System.currentTimeMillis());
 
+		_db = DBManagerUtil.getDB();
+
 		setUpUpgradeKaleoDefinitionVersion();
 	}
 
@@ -118,12 +120,10 @@ public class UpgradeKaleoDefinitionVersionTest {
 	}
 
 	protected void deleteKaleoDefinitionVersion(String name) throws Exception {
-		DB db = DBManagerUtil.getDB();
-
-		db.runSQL(
+		_db.runSQL(
 			"delete from KaleoDefinitionVersion where name = '" + name + "'");
 
-		db.runSQL("delete from KaleoDefinition where name = '" + name + "'");
+		_db.runSQL("delete from KaleoDefinition where name = '" + name + "'");
 	}
 
 	protected KaleoDefinitionVersion getKaleoDefinitionVersion(
@@ -169,6 +169,8 @@ public class UpgradeKaleoDefinitionVersionTest {
 
 			});
 	}
+
+	private DB _db;
 
 	@DeleteAfterTestRun
 	private Group _group;